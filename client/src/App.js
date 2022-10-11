df3a634ce14935a58fd5887856f42f3be97a070e
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-11 16:20:57 +0300

    Add routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import User from "./components/User";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +24,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <Router>
+        <Routes>
+          <Route
+            path="/users"
+            element={User}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
