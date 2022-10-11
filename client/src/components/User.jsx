f83e92e019de858d52cb06f7d928e42b163af54a
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-11 19:21:41 +0300

    test routing

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 function User() {
   useEffect(() => {
     fetch("/users")
-      .then((response) => response.json)
+      .then((response) => response.json())
       .then((data) =>
         console.log("I am not sure if we got any users yet...", data)
       );
