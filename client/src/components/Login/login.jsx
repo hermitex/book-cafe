aaeee26268845fb1da1d73087fa082700b9435e1
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-10-12 10:57:06 +0300

    login design

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import "./login.css"
+import LOGO from "../../assets/login.png"
+import {FaUserCheck} from "react-icons/fa"
+import {FaKey} from "react-icons/fa"
+
+
 
 const Login = ( ) => {
     return (
         <div className="login">
+                <div className="dash">
                 <h1>BOOK CAFE</h1>
+                <img src = { LOGO} alt="logo" />
+
+        <div className="form">
+             <FaUserCheck /><input type="text" name="name" placeholder="Enter Your Name" required />
+                <FaKey /><input type="email" name="email" placeholder="Enter Password........" required />
+         </div>
+
+         <div className="button">
+                <button type="submit" className="btn btn-primary">Submit</button>
         </div>
+
+        <div className="signup">
+                <h2>Have no account? <span>Sign Up</span></h2>
+        </div>
+
+        <div className="copyright">
+            <small>&copy; 2022 Nairobi <span> KENYA. </span> All rights reserved</small>
+        </div>
+
+                </div>
+                </div>
+
+
+
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import "./App.jsx"
 import "./index.css"
 import App from "./App.jsx";
+import "./App"
 
 //it takes two arguments the HTML code and the element hence it displays the HTML code inside the specified element.
 ReactDOM.render(< App/>, document.querySelector("#root"))
