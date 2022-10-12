b30411a7fc6f6f8bc237ce2694229bfe8234f907
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-10-12 23:08:00 +0300

    added the onclick event attribute

---
+++ client/src/components/login/Login.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Login.css";
+import logo from "../../assets/login.png";
+import Signup from "../signup/Signup";
+
+const Login = () => {
+  return (
+    <div className="main">
+      <div className="sub-main">
+        <div className="login">
+          <div className="dash">
+            <h1>BOOK CAFE</h1>
+            <img
+              src={logo}
+              alt="logo"
+            />
+
+            <div className="form">
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter UserName................"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Password............"
+                required
+              />
+              <input
+                type="checkbox"
+                name="password"
+                placeholder="Remember Username..........."
+                required
+              />
+            </div>
+
+            <div className="button">
+              <button
+                type="submit"
+                className="btn btn-primary"
+              >
+                Submit
+              </button>
+            </div>
+
+            <div className="signup">
+              <h2>
+                Have no account?{" "}
+                <button onClick={Signup}
+                  type="button"
+                  className="btn">
+                  Sign Up
+                </button>
+              </h2>
+            </div>
+
+            <div className="copyright">
+              <small>
+                &copy; 2022 Nairobi <span> KENYA. </span> All rights reserved
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/components/signup/Signup.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Signup.css";
+import Login from "../login/Login";
+
+const Signup = () => {
+  return (
+    <div className="main">
+    <div className="sub-main">
+      <div className="sign">
+        <h1>SIGN UP</h1>
+      </div>
+      <div className="signup">
+              <h2> Have an account?{" "}
+                <button onClick={Login} type="button" className="btn">
+                    Login
+                </button>
+              </h2>
+            </div>
+
+            <div className="form">
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your username................"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter you email............"
+                required
+              />
+              <input
+              type="text"
+              name="name"
+              placeholder="Nairobi..........."
+              required
+            />
+
+            <input
+              type="number"
+              name="phone"
+              placeholder="+254 123 456 789........."
+              required
+            />
+              <input
+                type="checkbox"
+                name="password"
+                placeholder="Remember Username..........."
+                required
+              />
+            </div>
+
+            <div className="button">
+              <button
+                type="submit"
+                className="btn btn-primary"
+              >
+                Submit
+              </button>
+            </div>
+
+            <div className="text">
+                <p>Register your book and swap it with a new one
+                or perhaps create a new book lover. Participating in a
+                book exchange also saves on ink and leaves a smaller
+                environmental footprint than printing a book.</p>
+            </div>
+
+            <div className="copyright">
+            <small>
+              &copy; 2022 Nairobi <span> KENYA. </span> All rights reserved
+            </small>
+          </div>
+
+      </div>
+      </div>
+  );
+};
+
+export default Signup;
