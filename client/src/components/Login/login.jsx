a56f6cf44fdd363c12de824f982a2bde24e44212
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-12 18:04:09 +0300

    fix package errors

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,46 +1,72 @@
 import React from "react";
-import "./login.css"
-import LOGO from "../../assets/login.png"
+import "./login.css";
+import LOGO from "../../assets/login.png";
 
-
-
-
-
-const Login = ( ) => {
-    return (
-        <div className="main">
-     <div className="sub-main">
+const Login = () => {
+  return (
+    <div className="main">
+      <div className="sub-main">
         <div className="login">
-                <div className="dash">
-                <h1>BOOK CAFE</h1>
-                <img src = { LOGO}  alt="logo" />
+          <div className="dash">
+            <h1>BOOK CAFE</h1>
+            <img
+              src={LOGO}
+              alt="logo"
+            />
 
-        <div className="form">
-             <input type="text" name="name" placeholder="Enter UserName................" required />
-            <input type="email" name="email" placeholder="Enter Password............" required />
-                <input type="checkbox" name="password" placeholder="Remember Username..........." required />
-         </div>
+            <div className="form">
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter UserName................"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Password............"
+                required
+              />
+              <input
+                type="checkbox"
+                name="password"
+                placeholder="Remember Username..........."
+                required
+              />
+            </div>
 
-         <div className="button">
-                <button type="submit" className="btn btn-primary">Submit</button>
+            <div className="button">
+              <button
+                type="submit"
+                className="btn btn-primary"
+              >
+                Submit
+              </button>
+            </div>
 
+            <div className="signup">
+              <h2>
+                Have no account?{" "}
+                <button
+                  type="button"
+                  class="btn"
+                  onclick="openSignUp()"
+                >
+                  Sign Up
+                </button>
+              </h2>
+            </div>
+
+            <div className="copyright">
+              <small>
+                &copy; 2022 Nairobi <span> KENYA. </span> All rights reserved
+              </small>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-        <div className="signup">
-                <h2>Have no account?  <button type="button" class="btn" onclick="openSignUp()">Sign Up</button></h2>
-        </div>
-
-        <div className="copyright">
-            <small>&copy; 2022 Nairobi <span> KENYA. </span> All rights reserved</small>
-        </div>
-
-                </div>
-                </div>
-</div>
-</div>
-
-
-    )
-}
-
-export default Login
+export default Login;
