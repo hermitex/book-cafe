d716ebb4bd4042f56ce7f31cb4e6b2afbe8973ef
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-12 22:39:06 +0300

    edit login form

---
+++ client/src/components/Copyright/Copyright.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Copyright() {
+  return (
+    <div className="copyright">
+      <small>
+        &copy; 2022 Nairobi <span> KENYA. </span> All rights reserved
+      </small>
+    </div>
+  );
+}
+
+export default Copyright;
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,66 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import logo from "../../assets/login.png";
+import logo from "../../assets/logo.png";
+import Copyright from "../Copyright/Copyright";
 
 const Login = () => {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [user, setUser] = useState(null);
+
+  const handleChange = (event) => {
+    const { name } = event.target;
+    const { value } = event.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    console.log(event);
+    event.preventDefault();
+    try {
+      const response = await fetch("/login");
+      const data = await response.json();
+      setUser(data);
+      setData({ username: "", password: "" });
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="main">
       <div className="sub-main">
         <div className="login">
           <div className="dash">
-            <h1>BOOK CAFE</h1>
-            <img
-              src={logo}
-              alt="logo"
-            />
+            <h1 className="login-title">BOOK CAFE</h1>
+            <div className="logo">
+              <img
+                width="100%"
+                src={logo}
+                alt="logo"
+              />
+            </div>
 
             <div className="form">
-              <input
-                type="text"
-                name="name"
-                placeholder="Enter UserName................"
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter Password............"
-                required
-              />
-              <input
-                type="checkbox"
-                name="password"
-                placeholder="Remember Username..........."
-                required
-              />
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Enter Username................"
+                  required
+                  value={data.username}
+                  onChange={handleChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter Password............"
+                  required
+                  value={data.password}
+                  onChange={handleChange}
+                />
+
+                <div className="button">
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                  >
+                    Submit
+                  </button>
+                </div>
+
+                <div className="signup">
+                  <small>
+                    Have no account?{" "}
+                    <button
+                      type="submit"
+                      className="btn"
+                    >
+                      Sign Up
+                    </button>
+                  </small>
+                </div>
+              </form>
             </div>
 
-            <div className="button">
-              <button
-                type="submit"
-                className="btn btn-primary"
-              >
-                Submit
-              </button>
-            </div>
-
-            <div className="signup">
-              <h2>
-                Have no account?{" "}
-                <button
-                  type="button"
-                  className="btn"
-                >
-                  Sign Up
-                </button>
-              </h2>
-            </div>
-
-            <div className="copyright">
-              <small>
-                &copy; 2022 Nairobi <span> KENYA. </span> All rights reserved
-              </small>
-            </div>
+            <Copyright />
           </div>
         </div>
       </div>
